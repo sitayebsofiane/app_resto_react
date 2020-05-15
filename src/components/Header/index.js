e41a79b3e485e706a0a62e5c017e0647b3dcc964
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-05-15 05:58:35 +0200

    mise ajour

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './../../App.css';
+
+const Header = () => {
+    return (
+        <header>
+          <div className="banner-container">
+            <h1><a href="/">Gestion RESTO</a></h1>
+          </div>  
+        </header>
+    )
+}
+export default Header;
