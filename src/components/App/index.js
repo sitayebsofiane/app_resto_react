f5c13262685b5dcc7fa7ac2be8d75c6c6bbef347
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-05-15 06:32:42 +0200

    creation de header landing et footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './../../App.css';
 import Header from '../Header';
+import Landing from '../Landing';
+import Footer from '../Footer';
 
 function App() {
   return (
     <div >
       <Header />
+      <Landing />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Project realiser par <a href="https://sitayebsofiane.github.io/cv_html_css/" target="_blanck">sofiane</a> 2020</p>
+                <p>Project realiser avec React js</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
