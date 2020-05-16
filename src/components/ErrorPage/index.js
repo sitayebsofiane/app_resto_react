54f5e5ebfc24002ccada0b5baf7d55b57d98be09
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-05-16 23:06:47 +0200

    ajout page d'erreur

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import batman from '../../images/batman.png';
 
 const ErrorPage = () => {
     return (
         <div className="resto-bg">
             <div className="container">
-                ErrorPage
+               <h2>Oups, cette page n'existe pas salam alikoum</h2>
+               <img src={batman} alt="error page" />
             </div>
         </div>
     )
