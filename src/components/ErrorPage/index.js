64d1961d4450a9f5ccbd17af6f31c93d7ea50c6a
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-05-17 17:21:11 +0200

    instalation firebase

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import batman from '../../images/batman.png';
+const centreH2 = {
+    textAlign: 'center',
+    marginTop: '50px'
+}
 
 const ErrorPage = () => {
     return (
         <div className="resto-bg">
             <div className="container">
-               <h2>Oups, cette page n'existe pas salam alikoum</h2>
-               <img src={batman} alt="error page" />
+               <h2 style={centreH2}>Oups, cette page n'existe pas salam alikoum</h2>
+               <img  src={batman} alt="error page" />
             </div>
         </div>
     )
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+const styleInscription ={
+    display:'inline-block',
+    padding: '100px'
+
+}
 const Welcome = () => {
     return (
         <div className="resto_bg">
             <div>
                 Welcome
             </div>
-            <div className="leftBox" >
+            <div style={styleInscription} className="leftBox" >
                 <Link to="/signup" className="btn-welcome">Inscription</Link>
             </div>
         </div>
